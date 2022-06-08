204e2da739b97b84113d55147b1ca9e9915a6c3f
Alejo <alejoivanrojas@gmail.com>
2022-06-08 00:01:27 -0300

    metadata added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,20 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 import Image from 'next/image'
 import people from "../public/people.jpg"
 
 const Home: NextPage = () => {
   return (
     <div className='landing'>
+      <Head>
+        <title>Digital House</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content='En Digital House somos una organización 𝘦𝘥𝘵𝘦𝘤𝘩 que ofrece 
+        distintas opciones de formación 100% a distancia sobre programación, diseño UX, datos y 
+        marketing digital. Entre ellas encontrarás una carrera, cursos intensivos, capacitaciones a 
+        empresas, programas con escuelas y más.' />
+        <link rel="shortcut icon" href="/logo.png" />
+      </Head>
       <div>
        <h1>More than education</h1>
        <h2>Share experiences</h2>
