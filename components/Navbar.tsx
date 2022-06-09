a201615132a61f4ba14dd5bdc2672e44ec274651
Alejo <alejoivanrojas@gmail.com>
2022-06-09 00:04:12 -0300

    responsive added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import logo from "../public/logo.png"
 import styles from "./navbar.module.css"
 import contentBR from "../locales/br"
@@ -8,19 +8,24 @@ import contentAR from "../locales/ar"
 import { Language } from '../pages/_app'
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false)
   const locale = useContext(Language)
   const content = locale === "es" ? contentAR : contentBR
   return (
     <nav className={styles.nav}>
-        <div  className={styles.logo}>
-            <Image src={logo} alt="logo" />
+        <div className={styles.header} >
+          <div  className={styles.logo}>
+            <Link href="/">
+              <Image src={logo} alt="logo" />
+            </Link>
+          </div>
+          <button onClick={()=>setOpen(!open)} className={styles.toggle} >X</button>
         </div>
-        <div className={styles.links}>
+        <div className={open ? styles.activeLinks : styles.links}>
             <Link href="/">{content.home}</Link>        
             <Link href="/about">{content.about}</Link>        
             <Link href="/contact">{content.contact}</Link>        
         </div>
-        
     </nav>
   )
 }
